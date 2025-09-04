951a71f71af7587a9b1b88d619fc2022eda35890
Youcef <youceflagraa970@gmail.com>
2025-09-04 18:57:25 +0200

    building vector store

---
+++ pipeline/build_pipeline.py
@@ -0,0 +1,27 @@
+from src.DataLoader import AnimeDataLoader
+from src.vector_store import VectorStoreBuilder
+from dotenv import load_dotenv
+from utils.logger import get_logger
+from utils.custom_exception import CustomException
+
+logger = get_logger()
+
+def main():
+    try:
+        logger.info("Starting to build pipeline")
+
+        loader = AnimeDataLoader(original_csv="../data/anime_with_synopsis.csv", processed_csv="../data/anime_updated.csv")
+        processed_data = loader.load_and_process()
+
+        logger.info("Data loaded and Processed...")
+
+        vector_builder = VectorStoreBuilder(csv_path=processed_data)
+        vector_builder.build_and_save_vector_store()
+
+        logger.info("Vector store built successfully !")
+
+        logger.info("pipeline built successfully!")
+
+    except Exception as e:
+        logger.error("Failed Building the pipeling ")
+        raise CustomException("Error during Building Pipeline")
