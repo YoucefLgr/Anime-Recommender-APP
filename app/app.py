0a34e0acccc21892719c05b1ecce577e96fb649f
Youcef <youceflagraa970@gmail.com>
2025-09-04 20:00:34 +0200

    project UI

---
+++ app/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from pipeline.pipeline import AnimeRecommendationPipeline
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title = "Anime Recommender", layout="wide")
+
+@st.cache_resource
+def init_pipeline():
+    return AnimeRecommendationPipeline()
+
+pipeline = init_pipeline()
+
+st.title("AI Anime Partner")
+
+query = st.text_input("Enter your favourite anime and preferences")
+
+if query:
+    with st.spinner("Fetching recommendations for you....."):
+        response = pipeline.recommend(query)
+        st.markdown("### Recommendations : ")
+        st.write(response)
